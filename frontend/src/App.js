28975aa194ea47a180f2c289f62902d173a441e2
Clement janot <clement.janot@edu.ece.fr>
2020-12-07 11:44:24 +0100

    reglage des problemes de responsivité

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,17 +6,24 @@ import Dashbb from './MonDashboard';
 import Formulaire from './components/Formulaire';
 import BarreDeNav from './components/Navigation';
 
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+
 
 function App() {
   return (
      <div className="col-12">
        <BrowserRouter>
         <div className="Placement">
+         
           <BarreDeNav />
+          
             <Switch>
              <Route path="/" component={Dashbb} exact/>
              <Route path="/formulaire" component={Formulaire}/>
            </Switch>
+           
         </div> 
       </BrowserRouter>
      </div>
