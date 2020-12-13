4d6b66bfee9c9816b246dc4c220ceed16611fd24
Clement janot <clement.janot@edu.ece.fr>
2020-12-13 20:38:49 +0100

    signup

--- frontend/src/components/Formulaire2.js
+++ frontend/src/components/Formulaire2.js
@@ -1,55 +1,71 @@
-import React from "react";
+import React, {Component}from "react";
 import './Formulaire2.css';
-
 import { Button, FormGroup, FormControl } from "react-bootstrap";
-import API from "./utils/APIform";
 
 import { ControlLabel } from 'rsuite';
+import axios from 'axios';
 
-export default class Signup extends React.Component {
-  state = {
-    email: "",
-    password: "",
-    cpassword: ""
-  };
-  send = async () => {
-    const { email, password, cpassword } = this.state;
-    if (!email || email.length === 0) return;
-    if (!password || password.length === 0 || password !== cpassword) return;
-    try {
-      const { data } = await API.signup({ email, password });
-      localStorage.setItem("token", data.token);
-      window.location = "/dashboard";
-    } catch (error) {
-      console.error(error);
+class Signup extends React.Component {
+  constructor(props){
+    super(props);
+
+      this.onChangeEmail = this.onChangeEmail.bind(this);
+      this.onChangePassword = this.onChangePassword.bind(this);
+      this.onSubmit=this.onSubmit.bind(this);
+        
+
+    this.state={
+      email:'',
+      password:''
     }
-  };
-  handleChange = (event) => {
+  }
+
+
+  onChangeEmail(e){
     this.setState({
-      [event.target.id]: event.target.value
+      email:e.target.value
     });
-  };
+  }
+
+    onChangePassword(e){
+      this.setState({
+        password:e.target.value
+      });
+  }
+  onSubmit(e){
+    e.preventDefault();
+    
+    const user={
+      email:this.state.email,
+      password:this.state.password
+    }
+
+    console.log(user);
+    //window.location='/'
+    axios.post('http://localhost:3002/auth/signup', user)
+      .then(res=>console.log(res.data))
+
+  }
   render() {
-    const { email, password, cpassword } = this.state;
     return (
-      <div className="signup">
-        <FormGroup controlId="email" bsSize="large">
-          <ControlLabel>Email</ControlLabel>
-          <FormControl autoFocus type="email" value={email} onChange={this.handleChange}/>
-        </FormGroup>
-
-        <FormGroup controlId="password" bsSize="large">
-          <ControlLabel>Password</ControlLabel>
-          <FormControl value={password} onChange={this.handleChange} type="password"/>
-        </FormGroup>
-
-        <FormGroup controlId="cpassword" bsSize="large">
-          <ControlLabel>Confirm Password</ControlLabel>
-          <FormControl value={cpassword} onChange={this.handleChange} type="password"/>
-        </FormGroup>
-
-        <Button type="submit" onClick={this.send} block bsSize="large" type="submit">Inscription</Button>
+      <div>
+        <h3>Signup</h3>
+        <form onSubmit={this.onSubmit}>
+          <div>
+            <div className="form-group">
+              <label>email : </label>
+              <input type="text"  value={this.state.email} onChange={this.onChangeEmail}/>
+            </div>
+            <div className="form-group">
+              <label>password : </label>
+              <input type="password"  value={this.state.password} onChange={this.onChangePassword}/>
+            </div>
+            <div className="form-group">
+              <input type="submit" className="btn btn-primary" value="create user"/>
+            </div>
+          </div>
+        </form>
       </div>
     );
   }
-}
+}export default Signup;
